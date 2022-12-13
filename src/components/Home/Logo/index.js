b24aab5b2bdec534962a550c402780f1053e8661
Merrill Shen <50651325+merrillshen@users.noreply.github.com>
2022-12-14 01:08:43 +0800

    Removed gsap-trial issues

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,37 +1,37 @@
 import './index.scss'
 import LogoS from '../../../assets/images/backgroundMS.png'
 import { useEffect, useRef } from 'react'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import gsap from 'gsap-trial'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import gsap from 'gsap-trial'
 
 const Logo = () => {
     const bgRef = useRef()
     const outlineLogoRef = useRef()
     const solidLogoRef = useRef()
     
-    useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
+    // useEffect(() => {
+    //     gsap.registerPlugin(DrawSVGPlugin)
 
-        gsap.timeline()
-        .to(bgRef.current,{
-            duration: 1,
-            opacity: 1,
-        })
-        .from(outlineLogoRef.current,{
-            drawSVG:0,
-            duration:20,
-        })
+    //     gsap.timeline()
+    //     .to(bgRef.current,{
+    //         duration: 1,
+    //         opacity: 1,
+    //     })
+    //     .from(outlineLogoRef.current,{
+    //         drawSVG:0,
+    //         duration:20,
+    //     })
 
-        gsap.fromTo(
-            solidLogoRef.current, {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
-    },[])
+    //     gsap.fromTo(
+    //         solidLogoRef.current, {
+    //             opacity: 0,
+    //         }, {
+    //             opacity: 1,
+    //             delay: 4,
+    //             duration: 4,
+    //         }
+    //     )
+    // },[])
 
     return (
         <div className='logo-container' ref={bgRef}>
